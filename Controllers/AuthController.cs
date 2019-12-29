3a90dbaf266ed5535ef137d10c7f6eb11cc2f497
GitHubMehdii <mehdi.mc60@gmail>
2019-12-29 21:11:15 +0100

    Adding AuthController

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using projectconventions.Models;
+
+namespace projectconventions.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        // GET api/auth
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "auth1", "auth1" };
+        }
+
+        // GET api/auth/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return "value " + id;
+        }
+
+        /*
+         * ------------------------------------------------------------------
+         * Login
+         * ------------------------------------------------------------------
+         */
+
+        // POST api/auth
+        [HttpPost]
+        public ActionResult<User> Post([FromBody] User user)
+        {
+
+            return new User(0, user.Username, user.Password);
+        }
+
+        // PUT api/auth/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/auth/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+
+    }
+}
