b3d0a835ae1c2af4c9636a25d2db50d1bd09583f
GitHubMehdii <mehdi.mc60@gmail>
2019-12-29 20:32:31 +0100

    Setting project

---
+++ Models/User.cs
@@ -0,0 +1,22 @@
+﻿
+namespace projectconventions.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+
+        public User()
+        {
+        }
+
+        public User(int Id, string Username, string Password)
+        {
+            this.Id = Id;
+            this.Username = Username;
+            this.Password = Password;
+        }
+
+    }
+}
