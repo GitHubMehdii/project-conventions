175dfa44bff5a82e9011a66923047a2f1d87aad7
Oussama Benloughmari <ben-oussama@hotmail.fr>
2019-12-31 10:45:27 +0100

    Email with pdf

--- Models/DB.cs
+++ Models/DB.cs
@@ -4,7 +4,7 @@ namespace project_conventions.Models
 {
     public class DB
     {
-        private static MySqlConnection Conn = new MySqlConnection("server=localhost;port=3306;database=conventions;user=root;password=root");
+        private static MySqlConnection Conn = new MySqlConnection("server=localhost;port=3306;database=conventions;user=root;password=");
 
         public static MySqlConnection GetConnection()
         {
