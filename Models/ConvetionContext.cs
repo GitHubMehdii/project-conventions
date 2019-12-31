5dfc47947fd3cc2b8160b3240ab3101e65fa72cc
GitHubMehdii <mehdi.mc60@gmail>
2019-12-31 11:09:18 +0100

    Conventions management

--- Models/ConvetionContext.cs
+++ Models/ConvetionContext.cs
@@ -12,25 +12,33 @@ namespace project_conventions.Models
         {
             MySqlConnection conn = DB.GetConnection();
 
-            conn.Open();
+            try
+            {
+                conn.Open();
 
-            string sqlCommand = "insert into conventions(apogee, startDate, EndDate, CompanyName, City,Comments, Status) values(" +
-                "'" + convention.Apogee + "'," +
-                "'" + convention.StartDate + "'," +
-                "'" + convention.EndDate + "'," +
-                "'" + convention.CompanyName + "'," +
-                "'" + convention.City + "'," +
-                "'" + convention.Comments + "'," +
-                "'" + convention.Status + "'" +
-            ")";
+                string sqlCommand = "insert into conventions(apogee, startDate, EndDate, CompanyName, City,Comments, Status) values(" +
+                    "'" + convention.Apogee + "'," +
+                    "'" + convention.StartDate + "'," +
+                    "'" + convention.EndDate + "'," +
+                    "'" + convention.CompanyName + "'," +
+                    "'" + convention.City + "'," +
+                    "'" + convention.Comments + "'," +
+                    "'" + convention.Status + "'" +
+                ")";
 
-            Console.WriteLine(sqlCommand);
+                MySqlCommand cmd = new MySqlCommand(sqlCommand, conn);
 
-            MySqlCommand cmd = new MySqlCommand(sqlCommand, conn);
+                var reader = cmd.ExecuteReader();
 
-            var reader = cmd.ExecuteReader();
-
-            conn.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error");
+            }
+            finally
+            {
+                conn.Close();
+            }
 
             return convention;
         }
@@ -41,29 +49,38 @@ namespace project_conventions.Models
 
             MySqlConnection conn = DB.GetConnection();
 
-            conn.Open();
+            try {
+                conn.Open();
 
-            string sqlCommand = "select * from conventions";
+                string sqlCommand = "select * from conventions";
 
-            MySqlCommand cmd = new MySqlCommand(sqlCommand, conn);
+                MySqlCommand cmd = new MySqlCommand(sqlCommand, conn);
 
-            var reader = cmd.ExecuteReader();
+                var reader = cmd.ExecuteReader();
 
-            while (reader.Read())
-            {
-                list.Add(new Convention(
-                    Convert.ToInt32(reader["Id"]),
-                    Convert.ToInt32(reader["Apogee"].ToString()),
-                    reader["StartDate"].ToString(),
-                    reader["EndDate"].ToString(),
-                    reader["CompanyName"].ToString(),
-                    reader["City"].ToString(),
-                    reader["Comments"].ToString(),
-                    reader["Status"].ToString()
-                ));
+                while (reader.Read())
+                {
+                    list.Add(new Convention(
+                        Convert.ToInt32(reader["Id"]),
+                        Convert.ToInt32(reader["Apogee"].ToString()),
+                        reader["StartDate"].ToString(),
+                        reader["EndDate"].ToString(),
+                        reader["CompanyName"].ToString(),
+                        reader["City"].ToString(),
+                        reader["Comments"].ToString(),
+                        reader["Status"].ToString()
+                    ));
+                }
+
             }
-
-            conn.Close();
+            catch (Exception e)
+            {
+                Console.WriteLine("Error");
+            }
+            finally
+            {
+                conn.Close();
+            }
 
             return list;
         }
@@ -76,29 +93,38 @@ namespace project_conventions.Models
 
             MySqlConnection conn = DB.GetConnection();
 
-            conn.Open();
+            try {
+                conn.Open();
 
-            string sqlCommand = "select * from conventions where apogee=" + apogee;
+                string sqlCommand = "select * from conventions where apogee=" + apogee;
 
-            MySqlCommand cmd = new MySqlCommand(sqlCommand, conn);
+                MySqlCommand cmd = new MySqlCommand(sqlCommand, conn);
 
-            var reader = cmd.ExecuteReader();
+                var reader = cmd.ExecuteReader();
 
-            while (reader.Read())
-            {
-                list.Add(new Convention(
-                    Convert.ToInt32(reader["Id"]),
-                    Convert.ToInt32(reader["Apogee"].ToString()),
-                    reader["StartDate"].ToString(),
-                    reader["EndDate"].ToString(),
-                    reader["CompanyName"].ToString(),
-                    reader["City"].ToString(),
-                    reader["Comments"].ToString(),
-                    reader["Status"].ToString()
-                ));
+                while (reader.Read())
+                {
+                    list.Add(new Convention(
+                        Convert.ToInt32(reader["Id"]),
+                        Convert.ToInt32(reader["Apogee"].ToString()),
+                        reader["StartDate"].ToString(),
+                        reader["EndDate"].ToString(),
+                        reader["CompanyName"].ToString(),
+                        reader["City"].ToString(),
+                        reader["Comments"].ToString(),
+                        reader["Status"].ToString()
+                    ));
+                }
+
             }
-
-            conn.Close();
+            catch (Exception e)
+            {
+                Console.WriteLine("Error");
+            }
+            finally
+            {
+                conn.Close();
+            }
 
             return list;
         }
@@ -107,35 +133,42 @@ namespace project_conventions.Models
         public Convention GetOneById(int id)
         {
             MySqlConnection conn = DB.GetConnection();
-
-            conn.Open();
-
-            string sqlCommand = "select * from conventions where Id=" + id;
-
-            Console.WriteLine(sqlCommand);
-
-            MySqlCommand cmd = new MySqlCommand(sqlCommand, conn);
-
-            var reader = cmd.ExecuteReader();
-
             Convention convention = null;
 
-            if (reader.Read())
-            {
-                convention = new Convention(
-                    Convert.ToInt32(reader["Id"]),
-                    Convert.ToInt32(reader["Apogee"].ToString()),
-                    reader["StartDate"].ToString(),
-                    reader["EndDate"].ToString(),
-                    reader["CompanyName"].ToString(),
-                    reader["City"].ToString(),
-                    reader["Comments"].ToString(),
-                    reader["Status"].ToString()
-                );
+            try {
+                conn.Open();
+
+                string sqlCommand = "select * from conventions where Id=" + id;
+
+                MySqlCommand cmd = new MySqlCommand(sqlCommand, conn);
+
+                var reader = cmd.ExecuteReader();
+
+
+                if (reader.Read())
+                {
+                    convention = new Convention(
+                        Convert.ToInt32(reader["Id"]),
+                        Convert.ToInt32(reader["Apogee"].ToString()),
+                        reader["StartDate"].ToString(),
+                        reader["EndDate"].ToString(),
+                        reader["CompanyName"].ToString(),
+                        reader["City"].ToString(),
+                        reader["Comments"].ToString(),
+                        reader["Status"].ToString()
+                    );
+                }
+
+
             }
-
-            conn.Close();
-
+            catch (Exception e)
+            {
+                Console.WriteLine("Error");
+            }
+            finally
+            {
+                conn.Close();
+            }
             return convention;
         }
 
@@ -144,24 +177,33 @@ namespace project_conventions.Models
         {
             MySqlConnection conn = DB.GetConnection();
 
-            conn.Open();
+            try {
+                conn.Open();
 
-            string sqlCommand = "update conventions set " +
-                "Apogee=" + convention.Apogee + ", " +
-                "StartDate='" + convention.StartDate + "', " +
-                "EndDate='" + convention.EndDate + "', " +
-                "CompanyName='" + convention.CompanyName + "', " +
-                "City='" + convention.City + "', " +
-                "Comments='" + convention.Comments + "', " +
-                "Status='" + convention.Status + "' " +
-                "where Id=" + convention.Id;
+                string sqlCommand = "update conventions set " +
+                    "Apogee=" + convention.Apogee + ", " +
+                    "StartDate='" + convention.StartDate + "', " +
+                    "EndDate='" + convention.EndDate + "', " +
+                    "CompanyName='" + convention.CompanyName + "', " +
+                    "City='" + convention.City + "', " +
+                    "Comments='" + convention.Comments + "', " +
+                    "Status='" + convention.Status + "' " +
+                    "where Id=" + convention.Id;
 
-            MySqlCommand cmd = new MySqlCommand(sqlCommand, conn);
+                MySqlCommand cmd = new MySqlCommand(sqlCommand, conn);
 
-            var reader = cmd.ExecuteReader();
+                var reader = cmd.ExecuteReader();
 
-            conn.Close();
 
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error");
+            }
+            finally
+            {
+                conn.Close();
+            }
             return convention;
         }
 
@@ -170,14 +212,23 @@ namespace project_conventions.Models
         {
             MySqlConnection conn = DB.GetConnection();
 
-            conn.Open();
+            try {
+                conn.Open();
 
-            string sqlCommand = "delete from conventions where Id=" + id;
+                string sqlCommand = "delete from conventions where Id=" + id;
 
-            MySqlCommand cmd = new MySqlCommand(sqlCommand, conn);
+                MySqlCommand cmd = new MySqlCommand(sqlCommand, conn);
 
-            var reader = cmd.ExecuteReader();
-            conn.Close();
+                var reader = cmd.ExecuteReader();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error");
+            }
+            finally
+            {
+                conn.Close();
+            }
         }
 
 
--- Models/UserContext.cs
+++ Models/UserContext.cs
@@ -11,27 +11,34 @@ namespace project_conventions.Models
         public User Save(User user)
         {
             MySqlConnection conn = DB.GetConnection();
+            try {
+                conn.Open();
 
-            conn.Open();
+                string sqlCommand = "insert into users values(" +
+                    user.Apogee + "," +
+                    "'" + user.BirthDate + "'," +
+                    "'" + user.FirstName + "'," +
+                    "'" + user.LastName + "'," +
+                    "'" + user.Email + "'," +
+                    "'" + user.Filiere + "'," +
+                    "'" + user.Year + "'," +
+                    "'" + user.About + "'," +
+                    "'" + user.IsAdmin + "'" +
+                ")";
 
-            string sqlCommand = "insert into users values(" +
-                user.Apogee + "," +
-                "'" + user.BirthDate + "'," +
-                "'" + user.FirstName + "'," +
-                "'" + user.LastName + "'," +
-                "'" + user.Email + "'," +
-                "'" + user.Filiere + "'," +
-                "'" + user.Year + "'," +
-                "'" + user.About + "'," +
-                "'" + user.IsAdmin + "'" +
-            ")";
+                MySqlCommand cmd = new MySqlCommand(sqlCommand, conn);
 
-            MySqlCommand cmd = new MySqlCommand(sqlCommand, conn);
+                var reader = cmd.ExecuteReader();
 
-            var reader = cmd.ExecuteReader();
-
-            conn.Close();
-
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error");
+            }
+            finally
+            {
+                conn.Close();
+            }
             return user;
         }
 
@@ -41,31 +48,39 @@ namespace project_conventions.Models
 
             MySqlConnection conn = DB.GetConnection();
 
-            conn.Open();
+            try {
+                conn.Open();
 
-            string sqlCommand = "select * from users where isAdmin = 'no'";
+                string sqlCommand = "select * from users where isAdmin = 'no'";
 
-            MySqlCommand cmd = new MySqlCommand(sqlCommand, conn);
+                MySqlCommand cmd = new MySqlCommand(sqlCommand, conn);
 
-            var reader = cmd.ExecuteReader();
+                var reader = cmd.ExecuteReader();
 
-            while (reader.Read())
-            {
-                list.Add(new User(
-                    Convert.ToInt32(reader["Apogee"]),
-                    reader["BirthDate"].ToString(),
-                    reader["FirstName"].ToString(),
-                    reader["LastName"].ToString(),
-                    reader["Email"].ToString(),
-                    reader["Filiere"].ToString(),
-                    reader["Year"].ToString(),
-                    reader["About"].ToString(),
-                    reader["IsAdmin"].ToString()
-                ));
+                while (reader.Read())
+                {
+                    list.Add(new User(
+                        Convert.ToInt32(reader["Apogee"]),
+                        reader["BirthDate"].ToString(),
+                        reader["FirstName"].ToString(),
+                        reader["LastName"].ToString(),
+                        reader["Email"].ToString(),
+                        reader["Filiere"].ToString(),
+                        reader["Year"].ToString(),
+                        reader["About"].ToString(),
+                        reader["IsAdmin"].ToString()
+                    ));
+                }
+
             }
-
-            conn.Close();
-
+            catch (Exception e)
+            {
+                Console.WriteLine("Error");
+            }
+            finally
+            {
+                conn.Close();
+            }
             return list;
         }
 
@@ -74,33 +89,42 @@ namespace project_conventions.Models
         {
             MySqlConnection conn = DB.GetConnection();
 
-            conn.Open();
-
-            string sqlCommand = "select * from users where Apogee=" + apogee;
-
-            MySqlCommand cmd = new MySqlCommand(sqlCommand, conn);
-
-            var reader = cmd.ExecuteReader();
-
             User user = null;
 
-            if (reader.Read())
+            try
             {
-                user = new User(
-                    Convert.ToInt32(reader["Apogee"]),
-                    reader["BirthDate"].ToString(),
-                    reader["FirstName"].ToString(),
-                    reader["LastName"].ToString(),
-                    reader["Email"].ToString(),
-                    reader["Filiere"].ToString(),
-                    reader["Year"].ToString(),
-                    reader["About"].ToString(),
-                    reader["IsAdmin"].ToString()
-                );
+                conn.Open();
+
+                string sqlCommand = "select * from users where Apogee=" + apogee;
+
+                MySqlCommand cmd = new MySqlCommand(sqlCommand, conn);
+
+                var reader = cmd.ExecuteReader();
+
+                if (reader.Read())
+                {
+                    user = new User(
+                        Convert.ToInt32(reader["Apogee"]),
+                        reader["BirthDate"].ToString(),
+                        reader["FirstName"].ToString(),
+                        reader["LastName"].ToString(),
+                        reader["Email"].ToString(),
+                        reader["Filiere"].ToString(),
+                        reader["Year"].ToString(),
+                        reader["About"].ToString(),
+                        reader["IsAdmin"].ToString()
+                    );
+                }
+
             }
-
-            conn.Close();
-
+            catch (Exception e)
+            {
+                Console.WriteLine("Error");
+            }
+            finally
+            {
+                conn.Close();
+            }
             return user;
         }
 
@@ -109,32 +133,41 @@ namespace project_conventions.Models
         {
             MySqlConnection conn = DB.GetConnection();
 
-            conn.Open();
-
-            string sqlCommand = "select * from users where Apogee=" + Apogee + " and BirthDate='" + BirthDate + "'";
-
-            MySqlCommand cmd = new MySqlCommand(sqlCommand, conn);
-
-            var reader = cmd.ExecuteReader();
-
             User user = null;
 
-            if (reader.Read())
+            try
             {
-                user = new User(
-                    Convert.ToInt32(reader["Apogee"]),
-                    reader["BirthDate"].ToString(),
-                    reader["FirstName"].ToString(),
-                    reader["LastName"].ToString(),
-                    reader["Email"].ToString(),
-                    reader["Filiere"].ToString(),
-                    reader["Year"].ToString(),
-                    reader["About"].ToString(),
-                    reader["IsAdmin"].ToString()
-                );
-            }
+                conn.Open();
 
-            conn.Close();
+                string sqlCommand = "select * from users where Apogee=" + Apogee + " and BirthDate='" + BirthDate + "'";
+
+                MySqlCommand cmd = new MySqlCommand(sqlCommand, conn);
+
+                var reader = cmd.ExecuteReader();
+
+                if (reader.Read())
+                {
+                    user = new User(
+                        Convert.ToInt32(reader["Apogee"]),
+                        reader["BirthDate"].ToString(),
+                        reader["FirstName"].ToString(),
+                        reader["LastName"].ToString(),
+                        reader["Email"].ToString(),
+                        reader["Filiere"].ToString(),
+                        reader["Year"].ToString(),
+                        reader["About"].ToString(),
+                        reader["IsAdmin"].ToString()
+                    );
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error");
+            }
+            finally
+            {
+                conn.Close();
+            }
 
             return user;
         }
@@ -144,24 +177,33 @@ namespace project_conventions.Models
         {
             MySqlConnection conn = DB.GetConnection();
 
-            conn.Open();
+            try {
+                conn.Open();
 
-            string sqlCommand = "update users set " +
-                "BirthDate='" + user.BirthDate + "', " +
-                "FirstName='" + user.FirstName + "', " +
-                "LastName='" + user.LastName + "', " +
-                "Email='" + user.Email + "', " +
-                "Filiere='" + user.Filiere + "', " +
-                "Year='" + user.Year + "', " +
-                "About='" + user.About + "', " +
-                "IsAdmin='" + user.IsAdmin + "' " +
-                "where Apogee=" + user.Apogee;
+                string sqlCommand = "update users set " +
+                    "BirthDate='" + user.BirthDate + "', " +
+                    "FirstName='" + user.FirstName + "', " +
+                    "LastName='" + user.LastName + "', " +
+                    "Email='" + user.Email + "', " +
+                    "Filiere='" + user.Filiere + "', " +
+                    "Year='" + user.Year + "', " +
+                    "About='" + user.About + "', " +
+                    "IsAdmin='" + user.IsAdmin + "' " +
+                    "where Apogee=" + user.Apogee;
 
-            MySqlCommand cmd = new MySqlCommand(sqlCommand, conn);
+                MySqlCommand cmd = new MySqlCommand(sqlCommand, conn);
 
-            var reader = cmd.ExecuteReader();
+                var reader = cmd.ExecuteReader();
 
-            conn.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error");
+            }
+            finally
+            {
+                conn.Close();
+            }
 
             return user;
         }
@@ -171,14 +213,25 @@ namespace project_conventions.Models
         {
             MySqlConnection conn = DB.GetConnection();
 
-            conn.Open();
+            try {
+                conn.Open();
 
-            string sqlCommand = "delete from users where Apogee=" + apogee;
+                string sqlCommand = "delete from users where Apogee=" + apogee;
 
-            MySqlCommand cmd = new MySqlCommand(sqlCommand, conn);
+                MySqlCommand cmd = new MySqlCommand(sqlCommand, conn);
 
-            var reader = cmd.ExecuteReader();
-            conn.Close();
+                var reader = cmd.ExecuteReader();
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error");
+            }
+            finally
+            {
+                conn.Close();
+            }
+
         }
 
 
