4b95d180bf7b6709735802f27cde7385d273a652
GitHubMehdii <mehdi.mc60@gmail>
2019-12-31 07:22:22 +0100

    CORS Config

--- Startup.cs
+++ Startup.cs
@@ -28,7 +28,7 @@ namespace project_conventions
         {
             // Make sure you call this before calling app.UseMvc()
             app.UseCors(
-                options => options.AllowAnyOrigin().AllowAnyMethod()
+                options => options.WithOrigins("*").AllowAnyOrigin().AllowAnyMethod()
             );
 
             if (env.IsDevelopment())
