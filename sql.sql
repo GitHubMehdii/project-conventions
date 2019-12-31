751ead4a8ac26555258fdee4761e1ffe0c147ff2
GitHubMehdii <mehdi.mc60@gmail>
2019-12-31 06:39:52 +0100

    CORS Config

--- sql.sql
+++ sql.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Dec 31, 2019 at 03:29 AM
+-- Generation Time: Dec 31, 2019 at 05:39 AM
 -- Server version: 5.6.38
 -- PHP Version: 7.2.1
 
@@ -28,15 +28,16 @@ CREATE TABLE `users` (
   `Email` varchar(30) NOT NULL,
   `Filiere` varchar(30) NOT NULL,
   `Year` varchar(30) NOT NULL,
-  `About` varchar(250) NOT NULL
+  `About` varchar(250) NOT NULL,
+  `isAdmin` varchar(5) NOT NULL DEFAULT 'no' COMMENT 'yes | no'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`Apogee`, `BirthDate`, `FirstName`, `LastName`, `Email`, `Filiere`, `Year`, `About`) VALUES
-(123, '', '', '', '', '', '', '');
+INSERT INTO `users` (`Apogee`, `BirthDate`, `FirstName`, `LastName`, `Email`, `Filiere`, `Year`, `About`, `isAdmin`) VALUES
+(12345678, '1995-01-01', 'Mehdi', 'Chaoui', 'mehdi.mc60@gmail.com', 'GI', '3', 'About me', 'no');
 
 --
 -- Indexes for dumped tables
