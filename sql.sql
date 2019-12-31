69c4b421af4f3f6758969650f8915a1d8ded1ecb
GitHubMehdii <mehdi.mc60@gmail>
2019-12-31 10:20:44 +0100

    Conventions management

--- sql.sql
+++ sql.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Dec 31, 2019 at 08:21 AM
+-- Generation Time: Dec 31, 2019 at 09:20 AM
 -- Server version: 5.6.38
 -- PHP Version: 7.2.1
 
@@ -28,8 +28,42 @@ CREATE TABLE `conventions` (
   `companyName` varchar(30) NOT NULL,
   `city` varchar(30) NOT NULL,
   `comments` varchar(250) NOT NULL,
-  `status` varchar(10) NOT NULL
+  `status` varchar(10) NOT NULL DEFAULT 'pending'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `conventions`
+--
+
+INSERT INTO `conventions` (`id`, `apogee`, `startDate`, `endDate`, `companyName`, `city`, `comments`, `status`) VALUES
+(1, 12345678, '2021-01-01', '2021-01-01', 'Berexia', 'Casablanca', 'Internship', 'refused');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `Apogee` int(11) NOT NULL,
+  `BirthDate` varchar(30) NOT NULL,
+  `FirstName` varchar(30) NOT NULL,
+  `LastName` varchar(30) NOT NULL,
+  `Email` varchar(30) NOT NULL,
+  `Filiere` varchar(30) NOT NULL,
+  `Year` varchar(30) NOT NULL,
+  `About` varchar(250) NOT NULL,
+  `IsAdmin` varchar(5) NOT NULL DEFAULT 'no' COMMENT 'yes | no'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`Apogee`, `BirthDate`, `FirstName`, `LastName`, `Email`, `Filiere`, `Year`, `About`, `IsAdmin`) VALUES
+(123, '1995-01-01', 'Mehdi', 'Chaoui', 'mehdi.mc60@gmail.com', 'GI', '3', 'About me', 'no'),
+(11111111, '1995-01-02', 'Mehdi', 'Chaoui', 'mehdi.mc60@gmail.com', 'GI', '3', 'About me', 'no'),
+(12345678, '1995-01-01', 'Mehdi', 'Chaoui', 'mehdi.mc60@gmail.com', 'GI', '3', 'About me', 'no');
 
 --
 -- Indexes for dumped tables
@@ -42,6 +76,12 @@ ALTER TABLE `conventions`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD UNIQUE KEY `apogee` (`Apogee`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -49,4 +89,4 @@ ALTER TABLE `conventions`
 -- AUTO_INCREMENT for table `conventions`
 --
 ALTER TABLE `conventions`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
